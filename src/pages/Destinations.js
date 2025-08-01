e3fe8276c1c46938466052c96e23315439ab4753
Rob and Trish <thewhitehairs@sbcglobal.net>
2025-08-01 10:49:05 -0400

    Change card layout from 3 to 4 cards per row for better screen utilization

--- src/pages/Destinations.js
+++ src/pages/Destinations.js
@@ -179,7 +179,7 @@ function Destinations() {
         </div>
 
         {/* Destinations Grid */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 items-stretch">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 items-stretch">
           {filteredDestinations.map((destination, index) => (
             <motion.div
               key={destination.id}
--- src/pages/SandalsBeaches.js
+++ src/pages/SandalsBeaches.js
@@ -296,7 +296,7 @@ function SandalsBeaches() {
         </div>
 
         {/* Resorts Grid */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 items-stretch">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 items-stretch">
           {filteredResorts.map((resort, index) => (
             <motion.div
               key={resort.id}
